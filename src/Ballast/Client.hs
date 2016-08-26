b36597bd36d3d3ba7e157fe557f0cac8238b8bf5
Alexey Zabelin <zabelin.alex@gmail.com>
2016-08-25 22:51:51 -0400

    Add functions to test the /rate endpoint
    This is a placeholder for now, used to check if the types I've been
    building work.

--- src/Ballast/Client.hs
+++ src/Ballast/Client.hs
@@ -1,6 +1,7 @@
 module Ballast.Client where
 
 import           Ballast.Types
+import           Data.Aeson                 (encode)
 import qualified Data.ByteString            as BS
 import qualified Data.ByteString.Char8      as BS8
 import qualified Data.ByteString.Lazy.Char8 as BSL
@@ -14,13 +15,18 @@ sandboxUrl = "https://api.beta.shipwire.com/api/v3"
 
 main :: IO ()
 main = do
-  manager <- newManager tlsManagerSettings
-  request <- parseRequest $ sandboxUrl ++ "/stock"
-  shipwireUser <- getEnv "SHIPWIRE_USER"
-  shipwirePass <- getEnv "SHIPWIRE_PASS"
-  let authorizedRequest =
-        applyBasicAuth (BS8.pack shipwireUser) (BS8.pack shipwirePass) request
-  response <- httpLbs authorizedRequest manager
-
-  putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus response)
-  BSL.putStrLn $ responseBody response
+    manager <- newManager tlsManagerSettings
+    initialRequest <- parseRequest $ sandboxUrl ++ "/rate"
+    let request =
+            initialRequest
+            { method = (BS8.pack "POST")
+            , requestBody = RequestBodyLBS $ encode defaultRate
+            }
+    shipwireUser <- getEnv "SHIPWIRE_USER"
+    shipwirePass <- getEnv "SHIPWIRE_PASS"
+    let authorizedRequest =
+            applyBasicAuth (BS8.pack shipwireUser) (BS8.pack shipwirePass) request
+    response <- httpLbs authorizedRequest manager
+    putStrLn $
+        "The status code was: " ++ (show $ statusCode $ responseStatus response)
+    BSL.putStrLn $ responseBody response
