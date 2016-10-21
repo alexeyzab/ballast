6fba126b1a948dc0424b1fea25d15a0b68a2292c
Alexey Zabelin <zabelin.alex@gmail.com>
2016-10-20 20:09:33 -0400

    Add initial tests for /receivings endpoint

--- tests/tests.hs
+++ tests/tests.hs
@@ -26,6 +26,34 @@ exampleShipTo =
     Commercial
     NotPoBox
 
+exampleCreateReceiving :: CreateReceiving
+exampleCreateReceiving =
+  CreateReceiving
+    Nothing
+    Nothing
+    (Just $ ExpectedDateText "2016-05-27T00:00:00-07:00")
+    (ReceivingOptions Nothing Nothing $ Just $ WarehouseRegion "TEST 1")
+    (ReceivingArrangement
+       ArrangementTypeNone
+       Nothing
+       Nothing)
+    (ReceivingShipments
+       [ReceivingShipment Nothing Nothing Nothing Nothing $ Type "box"])
+    Nothing
+    Nothing     
+    (ReceivingItems [ReceivingItem (SKU "Ballasttest") (Quantity 3)])
+    (ReceivingShipFrom
+       Nothing
+       (Name "Stephen Alexander")
+       (AddressLine "11 Prsilla St")
+       Nothing
+       (City "New York")
+       (State "NY")
+       (PostalCode "12345")
+       (Country "TestCountry")
+       (Phone "12346"))
+    Nothing
+        
 main :: IO ()
 main = hspec $ do
   describe "get rates" $ do
@@ -42,4 +70,14 @@ main = hspec $ do
     it "gets stock info with optional args" $ do
       config <- sandboxEnvConfig
       result <- shipwire config $ getStockInfo -&- (SKU "Ballasttest")
+      result `shouldSatisfy` isRight
+  describe "get receivings" $ do
+    it "gets an itemized list of receivings without optional args" $ do
+      config <- sandboxEnvConfig
+      result <- shipwire config $ getReceivings
+      result `shouldSatisfy` isRight
+  describe "create a new receivings" $ do
+    it "creates a new receiving without optional args" $ do
+      config <- sandboxEnvConfig
+      result <- shipwire config $ createReceiving exampleCreateReceiving
       result `shouldSatisfy` isRight
